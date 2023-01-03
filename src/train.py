4e8bc68fe999b6cd6d70231af7d29697790aff8b
Your Name <yukizaru.2587@icloud.com>
2023-01-03 15:08:45 +0900

    edit code

--- src/train.py
+++ src/train.py
@@ -82,11 +82,11 @@ def trainAug()
             transforms.RandomAffine(translate = (0, 0.5))
     ])
     
-    image_datagen = ImageFolder("path",transform) 
+    image_datagen = ImageFolder("path", transform) 
 
-#########################data augmentation(new2!!!!!)############################
+#########################data augmentation(new2!!!!!)###########################
 
-'''
+"""
 class MyTransforms:
     def __init__(self) -> None:
         pass
@@ -94,7 +94,7 @@ class MyTransforms:
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
         x = torch.from_numpy(x.astype(np.float32))
         return x
-'''
+"""
 
 class MyDataset(Dataset):
     def __init__(self, root: str, transforms) -> None:
@@ -140,6 +140,41 @@ dataloader = DataLoader(
     batch_size=64,
     shuffle=True
 )
+
+##############################dataset###############################
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):  #初期化
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self): #データの個数を返す
+        return len(self.img_labels)
+
+    def __getitem__(self, idx): #__len()__で返却された数値分のiが処理される for i in range()のような感じ
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+# Display image and label.
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap="gray")
+plt.show()
+print(f"Label: {label}")
 
 ##########################training models##############################
 

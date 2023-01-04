1af6042f1d74e8c98458c84457c930b782ec2690
Your Name <yukizaru.2587@icloud.com>
2023-01-04 17:55:39 +0900

    edit code

--- src/train2.py
+++ src/train2.py
@@ -1,14 +1,15 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import PIL
+from PIL import Image
 import torch
 import torch.nn as nn
 import torchvision
+import random
 
 class MyDataset(torch.utils.data.Dataset):
 
-    def __init__(self, label_path, transform=None):
+    def __init__(self, label_path, transform=None, transform_label=None):
         x = []
         y = []
     
@@ -22,30 +23,45 @@ class MyDataset(torch.utils.data.Dataset):
         self.y = torch.from_numpy(np.array(y)).float().view(-1, 1)
      
         self.transform = transform
-  
+        self.transform_label = transform_label
   
     def __len__(self):
         return len(self.x)
   
-  
     def __getitem__(self, i):
-        img = PIL.Image.open(self.x[i]).convert('RGB')
+
+        def regAug(img):
+            num = random.uniform(-150, 0)
+            img_rotate = img.rotate(0, translate=(0, num))
+            return img_rotate, num
+
+        img = Image.open(self.x[i]).convert('RGB')
 
         if self.transform is not None:
-            img = self.transform(img)
-    
+            img_rotate, num = regAug(img)
+            
+            file_name = os.path.basename(self.x[i]) 
+            name = str(i) + file_name 
+            PATH = os.path.join("../graph", name)
+            img_rotate.save(PATH)
+
+            img = self.transform(img_rotate)
+            self.y[i] = self.y[i] - num
+            
+            #print("{}:{}:{},{}".format(i, name, num, self.y[i]))
         return img, self.y[i]
 
 transform = torchvision.transforms.Compose([
-     torchvision.transforms.ToTensor(),
-     torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
+
 
 train_data_dir = '/mnt/data1/kikuchi/kikuchisan/reg/random_rotate_20/train/train_coodinate.tsv'
 valid_data_dir = '/mnt/data1/kikuchi/kikuchisan/reg/random_rotate_20/val/val_coodinate.tsv'
 
 trainset = MyDataset(train_data_dir, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=8, shuffle=True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=8, shuffle=False)
 
 validset = MyDataset(valid_data_dir, transform=transform)
 validloader = torch.utils.data.DataLoader(validset, batch_size=8, shuffle=False)
@@ -58,14 +74,14 @@ device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
 net = net.to(device)
 
 
-optimizer = torch.optim.SGD(net.parameters(), lr=0.00001)
+optimizer = torch.optim.SGD(net.parameters(), lr=0.001)
 criterion = torch.nn.MSELoss()
 
 train_loss = []
 valid_loss = []
 
-for epoch in range(50):
-    # 学習
+for epoch in range(10):
+    # train 
     net.train()
     running_train_loss = 0.0
 
@@ -81,9 +97,10 @@ for epoch in range(50):
             loss.backward()
             optimizer.step()
 
+
     train_loss.append(running_train_loss / len(trainset))
   
-    # 検証
+    # val
     net.eval()
     running_valid_loss = 0.0
 
